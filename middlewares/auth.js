259bdcc9d60e74a26c819aa878d4c4c865587011
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-03-07 22:07:23 +0100

    Changed middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,43 +1,14 @@
 const jwt = require('jsonwebtoken');
-const {
-  celebrate,
-  Joi,
-  Segments,
-} = require('celebrate');
-const router = require('express').Router();
 
 const { UnauthorizedError } = require('../errors');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
-const { login, createUser } = require('../controllers/users');
-
-router.post('/signup', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().required().uri(),
-  }),
-}), createUser);
-router.post('/signin', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
 
 module.exports = (req, res, next) => {
-  let { authorization } = req.headers;
+  const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    const { jwt: token } = req.cookies;
-
-    if (!token) {
-      throw new UnauthorizedError();
-    }
-
-    authorization = token;
+    throw new UnauthorizedError();
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -46,9 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new UnauthorizedError();
   }
 
   req.user = payload;
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,19 +1,7 @@
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
 const router = require('express').Router();
 
 const auth = require('./auth');
-const { requestLogger, errorLogger } = require('./logger');
 
-router.use(requestLogger);
-router.use(cors());
-router.use(helmet());
-router.use(cookieParser());
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({ extended: true }));
 router.use(auth);
-router.use(errorLogger);
 
 module.exports = router;
