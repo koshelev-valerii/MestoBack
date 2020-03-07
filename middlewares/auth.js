ead6a86b4ea5fac0d21b4277c5d111fcd4e4a881
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-03-07 02:03:55 +0100

    Changed middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,43 @@
 const jwt = require('jsonwebtoken');
+const {
+  celebrate,
+  Joi,
+  Segments,
+} = require('celebrate');
+const router = require('express').Router();
+
+const { UnauthorizedError } = require('../errors');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
+const { login, createUser } = require('../controllers/users');
+
+router.post('/signup', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().required().uri(),
+  }),
+}), createUser);
+router.post('/signin', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  let { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    const { jwt: token } = req.cookies;
+
+    if (!token) {
+      throw new UnauthorizedError();
+    }
+
+    authorization = token;
   }
 
   const token = authorization.replace('Bearer ', '');
