d276b038339834f210c6f6ef528daad71532ac93
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-03-08 22:39:30 +0100

    Changed auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError();
+    throw new UnauthorizedError('Неверный email или пароль');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError();
+    throw new UnauthorizedError('Неверный email или пароль');
   }
 
   req.user = payload;
